9c47450e5f93ec315622c25d73590791307e63bd
Sreeni1204 <hvsreenivasa93@gmail.com>
2025-09-08 12:44:55 +0530

    Add post request example to the live app

---
+++ post_request.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+
+data = [
+    {
+        "age": 52,
+        "workclass": "Self-emp-not-inc",
+        "fnlgt": 209642,
+        "education": "HS-grad",
+        "education_num": 9,
+        "marital_status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital_gain": 0,
+        "capital_loss": 0,
+        "hours_per_week": 45,
+        "native_country": "United-States"
+    }
+]
+
+response = requests.post('https://scalable-ml-pipeline.onrender.com/predict', data=json.dumps(data))
+
+print(response.status_code)
+print(response.json()["predicted_salary"])
